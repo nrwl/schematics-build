d4893696d3cf168fb8bf70004732d14f76e5eb5c
vsavkin <avix1000@gmail.com>
2017-10-12 10:19:28 -0400

    publishing 0.0.7+e6598c2

--- src/utility/lib-versions.d.ts
+++ src/utility/lib-versions.d.ts
@@ -2,8 +2,8 @@ export declare const angularCliVersion = "nrwl/fix-cli-build";
 export declare const angularVersion = "^4.4.4";
 export declare const angularJsVersion = "1.6.6";
 export declare const ngrxVersion = "^4.0.0";
-export declare const nxVersion = "nrwl/nx-build";
-export declare const schematicsVersion = "nrwl/schematics-build";
+export declare const nxVersion = "^0.0.6";
+export declare const schematicsVersion = "^0.0.6";
 export declare const prettierVersion = "1.7.4";
 export declare const libVersions: {
     angularVersion: string;
--- src/utility/lib-versions.js
+++ src/utility/lib-versions.js
@@ -4,8 +4,8 @@ exports.angularCliVersion = 'nrwl/fix-cli-build';
 exports.angularVersion = '^4.4.4';
 exports.angularJsVersion = '1.6.6';
 exports.ngrxVersion = '^4.0.0';
-exports.nxVersion = 'nrwl/nx-build';
-exports.schematicsVersion = 'nrwl/schematics-build';
+exports.nxVersion = '^0.0.6';
+exports.schematicsVersion = '^0.0.6';
 exports.prettierVersion = '1.7.4';
 exports.libVersions = {
     angularVersion: exports.angularVersion,
